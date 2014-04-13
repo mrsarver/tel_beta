367381d8b11018a1da051a0108ef7d1b277452c1
M. R. Sarver <msarver2010@gmail.com>
2014-04-12 22:30:26 -0400

    the big gui update

--- src/gui.py
+++ src/gui.py
@@ -1,91 +1,112 @@
 from Tkinter import *
-import tkMessageBox
+from PIL import Image, ImageTk
 from threading import *
-import camera
-
-###############################################################################
-#PROGRAM START
-###############################################################################
+import tkMessageBox
+import re
+import cv2
 
 class Application(Frame):
 	def createWidgets(self):
 		"""Construct Graphical User Interface Buttons"""
-		self.QUIT = Button(self)
-		self.QUIT['text'] = "quit"
-		self.QUIT['command'] = self.exit
+		self.IPLABEL = Label(self, text = "Src IPv4 Address:")
 
-		self.NEW = Button(self)
-		self.NEW['text'] = "New Call to IP"
-		self.NEW['command'] = self.newCall
+		self.IPENTRY = Entry(self, width=15)
+		self.IPENTRY.focus_set()
 
-		self.END = Button(self)
-		self.END['text'] = "End Call"
-		self.END['command'] = self.endCall
+		self.NEW = Button(self, text="New Call to IP", command=self.newCall)
+		self.END = Button(self, text="End Call", command=self.endCall)
+		self.QUIT = Button(self, text="Quit", command=self.exit)
+		self.FRAME = Label(self, image=self.picture)
 
-		self.QUIT.pack({'side':'left'})
-		self.NEW.pack({'side':'left'})
-		self.END.pack({'side':'left'})
+		self.IPLABEL.grid(row=0, column=0, sticky=W)
+		self.IPENTRY.grid(row=0, column=1, columnspan=2)
+		self.NEW.grid(row=1, column=0, sticky=W+E)
+		self.END.grid(row=1, column=1, sticky=W+E)
+		self.QUIT.grid(row=1, column=2, sticky=W+E)
+		self.FRAME.grid(row=2, column=0, columnspan=3)
+
+		self.appMenu = Menu(self)
+		self.fileMenu = Menu(self.appMenu, tearoff=0)
+		self.fileMenu.add_command(label="New Call", command=self.newCall)
+		self.fileMenu.add_command(label="End Call", command=self.endCall)
+		self.fileMenu.add_separator()
+		self.fileMenu.add_command(label="Exit", command=self.exit)
+		self.appMenu.add_cascade(label="File", menu=self.fileMenu)
 
 	def newCall(self):
 		"""Initialize a new call to a specified IP"""
-		# Start new chat opens message box (input src ip and button to start)"
-		#	REMEMBER: This message is a dummy message for now"
-		if not self.videoThread.isAlive():
-			tkMessageBox.askokcancel("New Call to IP",  "DUDE ENTER AN IP ADDRESS BRO!")
-			self.videoThread.start()
+		ip = self.IPENTRY.get()
+
+		if self.isValidIP(ip) == None or ip == "0.0.0.0":
+			tkMessageBox.showerror("Invalid IPv4 address", ip + "is an invalid IPv4 address.  Please enter a valid IPv4 address.")
+			self.IPENTRY.focus_set()
+			self.IPLABEL["fg"] = "red"
+			self.FRAME["image"] = self.derp
+			return False
+
+		self.IPLABEL["fg"] = "black"
+
+		if self.VideoThread == None:
+			self.VideoThread = Thread(target = self.webcamCap)
+			self.VideoThread.daemon = True
+			self.callActive = True
+			self.VideoThread.start()
+			return True
+		else:
+			tkMessageBox.showerror("End Current Call",  "Please end the current call before making a new one")
+			return False
 
 	def endCall(self):
 		"""End the current call"""
-		print "Bye world!"
-		self.videoThread.join()
+		if self.VideoThread == None:
+			tkMessageBox.showerror("No Current Call",  "No current call to end")
+			return False
+
+		self.callActive = False
+		self.VideoThread.join()
+		self.VideoThread = None
+		self.FRAME["image"] = self.picture
 
 	def exit(self):
 		"""Proper exiting and resource management"""
-		if tkMessageBox.askokcancel("Quit?", "Are you sure you want to exit.  This will end any calls you are making."):
-			if self.videoThread.isAlive():
-				#there is a problem here, you cannot restart a thread
-				self.endCall()
+		if tkMessageBox.askyesno("Quit?", "Are you sure you want to exit?  This will end any calls you are making."):
+			self.endCall()
 			self.quit()
+
+	def isValidIP(self, ip):
+		"""Returns None if invalid, match object otherwise"""
+		#the magic begins here
+		p = re.compile('^(([1-9]?\d|1\d\d|25[0-5]|2[0-4]\d)\.){3}([1-9]?\d|1\d\d|25[0-5]|2[0-4]\d)$')
+		return p.match(ip)
+
+	def webcamCap(self):
+		self.cap = cv2.VideoCapture(0)
+		#i feel ashamed of this loop
+		while self.callActive:
+			self.ret, self.img = self.cap.read()
+			self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
+			#if self.rotate == True:
+				#self.img.transpose(Image.FLIP_LEFT_RIGHT)
+			self.img = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+			self.FRAME["image"] = self.img
+			self.FRAME._image_cache = self.img
+		self.cap.release()
+
 
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
-		self.check = 0
-		self.videoThread = Thread(target = camera.function)
-		self.pack()
+		self.picture = ImageTk.PhotoImage(Image.open("telsmall.png"))
+		self.derp = ImageTk.PhotoImage(Image.open("derp.jpg"))
 		self.createWidgets()
+		self.VideoThread = None
+		self.callActive = False
+		self.rotate = True
+		self.grid()
 
 if __name__ == "__main__":
-	root = Tk()
-	root.title("Tel v0.0.0.1")
-	app = Application(master=root)
-	#bug site 1 - closing the window - close it with quit for now
-	root.protocol("WM_DELETE_WINDOW", app.exit)
-	#root.wm_iconbitmap(bitmap = "")
+	app = Application()
+	app.master.title("Tel v 0.0.1")
+	#app.master.maxsize(512, 512)
+	app.master.config(menu=app.appMenu)
+	app.master.protocol("WM_DELETE_WINDOW", app.exit)
 	app.mainloop()
-
-# In main
-# Initialize TK inter through wrapper class - let mainloop do its thing
-# Draw gui, startup buttons (spawn thread/child process), button functions, main method
-#
-# Starts OpenCV video capture in new thread, accessible through wrapper class
-#		-New thread handles internet connection
-#		-client-server (it'll be easier)
-#			-peer to peer later gater
-#
-# Wrapper class functions:
-#	1. Get Frame
-#	2. Send Frame
-#	3. Recieve Frame
-#	4. Show recieved Frame
-#	5. Has main method
-#
-# Connection Class:
-# Opens UDP connection to client PROGRAM
-#	1. Open Connection
-#	2. Close Connection
-#	3. Output frame to TCP Connection
-#	4. Recieve frame to TCP Connection
-#
-###############################################################################
-#PROGRAM END
-###############################################################################
